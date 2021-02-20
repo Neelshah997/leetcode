f35642e94325c9513c99e42ea64f68fb37e7bc16
Neel Shah <66415331+Neelshah997@users.noreply.github.com>
2021-02-20 17:06:14 +0530

    Time: 32 ms (68.98%), Space: 14 MB (97.29%) - LeetHub

--- fraction-addition-and-subtraction/fraction-addition-and-subtraction.py
+++ fraction-addition-and-subtraction/fraction-addition-and-subtraction.py
@@ -4,16 +4,9 @@ class Solution:
     def fractionAddition(self, expression: str) -> str:
         sign = 1
         n = len(expression)
+        div = False
         numerator = []
         denominator = []
-        def find_gcd(x, y): 
-            while(y): 
-                x, y = y, x % y 
-            return x 
-        prev = 0
-        gcd = 1
-        index = -1
-        div = False
         val = ""
         for i in range(n):
             if expression[i] == "-":
@@ -32,7 +25,6 @@ class Solution:
                 div = True
             else:
                 val+=expression[i]
-            print(val)
         denominator.append(int(val))
         x=0
         y=1
